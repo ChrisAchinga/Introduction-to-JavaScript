ecb1aabd526132ff0f8aa90bd815bcaebc7ecbde
ChrisAchinga <achinga.chris@gmail.com>
2020-03-02 18:20:33 +0300

    /

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Hello world")
+
+let number = 9292;
+let word = "I am me";
+console.log(number)
+console.log(word)
+
+// var
+// let
+// const
+
+function name() {
+    let intro = "My name is cris";
+    console.log(intro)
+
+}
+
+name();
